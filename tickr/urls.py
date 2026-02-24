f7d8415d7515b296a96a579dba69043942cc9811
Sravan <miniclip1956@gmail.com>
2026-02-24 15:17:31 +0530

    fix: Serve media files in all environments by moving configuration out of the debug block.
    Signed-off-by: Sravan <miniclip1956@gmail.com>

--- tickr/urls.py
+++ tickr/urls.py
@@ -47,7 +47,8 @@ urlpatterns = [
     path("checkin/", include("checkin.urls")),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
