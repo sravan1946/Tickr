9f03a4f2a422c2d0c661f28c0f34481e53884d2e
Sravan <miniclip1956@gmail.com>
2026-02-24 13:20:53 +0530

    fix: Update STATIC_ROOT path to 'static' in settings.
    Signed-off-by: Sravan <miniclip1956@gmail.com>

--- tickr/settings.py
+++ tickr/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Media files (user uploaded images)
 MEDIA_URL = "/media/"
