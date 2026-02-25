424b4a58daf25f1443911b92fa1986eb523069c9
Sravan <miniclip1956@gmail.com>
2026-02-25 14:11:06 +0530

    refactor: update event creation form action to redirect to the event list.
    Signed-off-by: Sravan <miniclip1956@gmail.com>

--- accounts/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpRequest
-
-from .auth import get_request_user
-
-
-def account_user(request: HttpRequest) -> dict[str, object]:
-    """Add account_user to template context for session-based auth."""
-    return {"account_user": get_request_user(request)}
